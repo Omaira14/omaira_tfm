088b8472f1cac64e2102f20c308ea99c4ff33c4e
Omaira14 <omairagp8@gmail.com>
2025-08-19 18:47:00 +0100

    push frontend only

--- frontend/index.service.js
+++ frontend/index.service.js
@@ -1,26 +1,12 @@
-// // Servicio para gestionar peticiones a la API
-// const API_URL = 'http://localhost:5000/api/buscar';
-
-// export async function buscarPersonas(descripcion, numResultados) {
-//   const response = await fetch(API_URL, {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({ descripcion, numResultados })
-//   });
-//   if (!response.ok) throw new Error('Error en la petición');
-//   return await response.json();
-// }
-
 // Servicio para gestionar peticiones a la API
-// En Azure y en local (con la CLI) será siempre /api/buscar
-const API_URL = '/api/buscar';
+const API_URL = 'http://localhost:5000/api/buscar';
 
 export async function buscarPersonas(descripcion, numResultados) {
   const response = await fetch(API_URL, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json'
+    },
     body: JSON.stringify({ descripcion, numResultados })
   });
   if (!response.ok) throw new Error('Error en la petición');
